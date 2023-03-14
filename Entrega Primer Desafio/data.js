17afb6c1f1db8b40ab5ab661aae1ce5891b764bb
LucasMedinaunu <85761862+LucasMedinaunu@users.noreply.github.com>
2023-03-14 11:06:58 -0300

    Delete Entrega Primer Desafio directory

--- Entrega Primer Desafio/data.js
+++
@@ -1,5 +0,0 @@
-const Bebida1 = { title: "Coca Cola", description: "Bebida Con Gas", price: 35, thumnail: "https://pizzaplace.com.ua/wp-content/uploads/2021/04/coca-cola-1l-400x400.jpg", code: 1, stock: 300}
-const Bebida2 = { title: "Paso De Los Toros", description: "Bebida Con Gas", price: 35, thumnail: "https://alfanea.com.ar/wp-content/uploads/2020/09/Paso-de-Los-Toros-Pomelo-sin-azucar-x-500-ml-450x450.jpg", code: 2, stock: 300}
-const Bebida3 = { title: "Sprite", description: "Bebida Con Gas", price: 35, thumnail: "https://th.bing.com/th/id/R.ed9da636e47424a4256f52b9f48d8b6b?rik=EcFxM8hdUsNwpA&pid=ImgRaw&r=0", code: 3, stock: 300}
-
-module.exports = { Bebida1, Bebida2, Bebida3 };
